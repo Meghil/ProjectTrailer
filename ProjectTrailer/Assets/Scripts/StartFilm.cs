b63885a632e116c604cf7875bca2e0e05a38991b
chrsz <chris.bomprezzi@gmail.com>
2018-07-10 13:23:01 +0200

    add first example of film

--- ProjectTrailer/Assets/Scripts/StartFilm.cs
+++ ProjectTrailer/Assets/Scripts/StartFilm.cs
@@ -1,26 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class StartFilm : MonoBehaviour
 {
+    public Text buttonUI;
 
-    //private BoxCollider wallCollider;
+    private VideoPlayer wallVideo;
 
 
     // Use this for initialization
     void Start()
     {
-        //wallCollider = GetComponent<BoxCollider>();
+        wallVideo = GetComponent<VideoPlayer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("F - Pressed!");
+            buttonUI.text = "F";
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            Debug.Log("G - Pressed!");
+            buttonUI.text = "G";
+        }
+        if (Input.GetKey(KeyCode.H))
+        {
+            Debug.Log("H - Pressed!");
+            buttonUI.text = "H";
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            Debug.Log("J - Pressed!");
+            buttonUI.text = "J";
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Video triggered D:");
+        wallVideo.Play();
     }
 }
