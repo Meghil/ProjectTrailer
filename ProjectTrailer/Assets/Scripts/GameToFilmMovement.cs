5fe48cbb5521d3b2e16b924a168b84ac99fc2a94
chrsz <chris.bomprezzi@gmail.com>
2018-07-10 12:44:48 +0200

    first commit film

---
+++ ProjectTrailer/Assets/Scripts/GameToFilmMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameToFilmMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    private Vector3 velocity;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = Vector3.zero;
+
+        velocity += Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime;
+        velocity += Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.position += velocity * speed;
+    }
+}
---
+++ ProjectTrailer/Assets/Scripts/StartFilm.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFilm : MonoBehaviour
+{
+
+    //private BoxCollider wallCollider;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //wallCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Video triggered D:");
+    }
+}
