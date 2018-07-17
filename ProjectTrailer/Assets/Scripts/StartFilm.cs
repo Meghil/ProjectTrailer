120b221bcde59a041282885cfa3716c0c26015ff
chrsz <chris.bomprezzi@gmail.com>
2018-07-17 15:27:43 +0200

    add progress bar and image buttons

--- ProjectTrailer/Assets/Scripts/StartFilm.cs
+++ ProjectTrailer/Assets/Scripts/StartFilm.cs
@@ -10,50 +10,38 @@ public class StartFilm : MonoBehaviour
     public Text videoTimeUI;
     public Text successBetweenUI;
     public GameObject secondCamera;
+    public GameObject pressXUI;
+    public GameObject pressBUI;
+    public GameObject pressYUI;
+    public Slider progressBar;
+    public float progressBarMaxValue;
+    public float progessBarSpeed;
 
     private VideoPlayer wallVideo;
+    private float progressBarCurrentValue;
 
 
     // Use this for initialization
     void Start()
     {
         wallVideo = GetComponent<VideoPlayer>();
+        progressBarCurrentValue = progressBarMaxValue;
+        progressBar.value = CalculateProgress();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.F))
-        {
-            Debug.Log("F - Pressed!");
-            buttonUI.text = "F";
-        }
-        if (Input.GetKey(KeyCode.G))
-        {
-            Debug.Log("G - Pressed!");
-            buttonUI.text = "G";
-        }
-        if (Input.GetKey(KeyCode.H))
-        {
-            Debug.Log("H - Pressed!");
-            buttonUI.text = "H";
-        }
-        if (Input.GetKey(KeyCode.J))
-        {
-            Debug.Log("J - Pressed!");
-            buttonUI.text = "J";
-        }
-
         if (wallVideo.isPlaying)
         {
             videoTimeUI.text = wallVideo.time.ToString();
 
-            if ((wallVideo.time > 5f && wallVideo.time < 6f) && Input.GetKey(KeyCode.Q))
-            {
-                Debug.Log("Pressed Q between 5 and 6");
-                successBetweenUI.text = "Pressed Q between 5 and 6";
-            }
+            PressButtonInTime(1f, 2f, KeyCode.Y, pressYUI);
+            PressButtonInTime(3f, 4f, KeyCode.X, pressXUI);
+            PressButtonInTime(5f, 6f, KeyCode.B, pressBUI);
         }
+
+        progressBar.value = Mathf.Lerp(progressBar.value, CalculateProgress(), Time.deltaTime * progessBarSpeed);
     }
 
     void OnTriggerEnter(Collider other)
@@ -63,5 +51,29 @@ public class StartFilm : MonoBehaviour
             secondCamera.SetActive(true);
             wallVideo.Play();
         }
+    }
+
+    void PressButtonInTime(float timeA, float timeB, KeyCode pressedButton, GameObject buttonImage)
+    {
+        if (wallVideo.time > timeA && wallVideo.time < timeB)
+        {
+            buttonImage.SetActive(true);
+            if (Input.GetKeyDown(pressedButton))
+            {
+                buttonUI.text = pressedButton.ToString();
+                Debug.Log(string.Format("Pressed {0} between {1} and {2}", pressedButton, timeA, timeB));
+                successBetweenUI.text = string.Format("Pressed {0} between {1} and {2}", pressedButton, timeA, timeB);
+                progressBarCurrentValue -= 1;
+            }
+        }
+        else
+        {
+            buttonImage.SetActive(false);
+        }
+    }
+
+    float CalculateProgress()
+    {
+        return progressBarCurrentValue / progressBarMaxValue;
     }
 }
